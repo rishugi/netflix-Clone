5b54cfa4921e769f1f5f4ebf523881e852dbfdbe
Rishu Gupt <rishugu298@gmail.com>
2025-01-24 12:57:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let name =getElementbyId("a")
+let number =getElementbyId("b")
+let dob =getElementbyId("c")
+let userId =getElementbyId("d")
+let password =getElementbyId("e")
+let rePassword =getElementbyId("f")
+
+
